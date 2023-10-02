a2582dc60925805b49f06bc8e9c4156e972513a8
uhamhz <ammarhafizh2404@gmail.com>
2023-10-02 19:35:48 +0700

    first commit

---
+++ Pemilihan2Percobaan117.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Pemilihan2Percobaan117
+ */
+public class Pemilihan2Percobaan117 {
+
+    public static void main(String[] args) {
+        Scanner input17 = new Scanner(System.in);
+        int tahun;
+
+        System.out.println("Masukan Tahun : ");
+        tahun = input17.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100)!= 0) {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}

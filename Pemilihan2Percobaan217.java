53776127f723a31539b0b112805b83e23be505b7
uhamhz <ammarhafizh2404@gmail.com>
2023-10-02 20:44:38 +0700

    Add Other Triangle Method

--- Pemilihan2Percobaan217.java
+++ Pemilihan2Percobaan217.java
@@ -18,9 +18,12 @@ public class Pemilihan2Percobaan217 {
         if (totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)){
                 System.out.println("Segitiga Tersebut Adalah Segitiga Siku-Siku");
-            }else{
-                System.out.println("Segitiga Tersebut Adalah Bukan Segitiga Siku-Siku");
-            }
+            }else if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60)){
+                System.out.println("Segitiga Tersebut Adalah Segitiga Sama Sisi");
+            }else if ((sudut1 == 120) || (sudut2 == 120) || (sudut3 == 120)){
+                System.out.println("Segitiga Tersebut Adalah Segitiga Tumpul");
+            }else if (sudut1 + sudut2 + sudut3 == 180);
+                System.out.println("Segitiga Tersebut Adalah Segitiga Sembarang");
         } else {
             System.out.println("Bukan Segitiga");
         }
